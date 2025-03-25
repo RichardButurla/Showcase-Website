60f5a85c69f6dea0d344d52c41801202e7d42c8a
RichardButurla <rj.buturla@gmail.com>
2025-03-25 22:16:48 +0000

    Added more content

--- js/projects.js
+++ js/projects.js
@@ -5,7 +5,6 @@ const projects = [
         media: [
             { type: "video", source: "DungeonTeaser.mp4", poster: "testVid.mp4" },
             { type: "video", source: "DungeonEditorTeaser.mp4" },
-            { type: "image", source: "/api/placeholder/600/400" }
         ],
         description: "This is a single-player, turn-based tactical RPG inspired by Baldur’s Gate 3, focusing on strategic combat with simplified mechanics. Players control heroes in a party,"
         + "exploring different environments and battling against different types of enemies, using a combination of movement, attacks, and abilities to outmaneuver and defeat foes. Key features"
@@ -19,9 +18,7 @@ const projects = [
         id: 2,
         title: "Endless Zombie Shooter",
         media: [
-            { type: "image", source: "/api/placeholder/600/400" },
-            { type: "video", source: "/api/placeholder/600/400", poster: "/api/placeholder/600/400" },
-            { type: "image", source: "/api/placeholder/600/400" }
+            { type: "video", source: "Endless Shooter.mp4" },
         ],
         description: "Within a group with another student, designed an endless shooter style game which showcased enemies who can attack and shoot at the player using different weapons and firing behaviours, "
         + "using a pathfinding algorithm to locate the player and AI attacking + steering behaviours. Implemented animations, particle effects and a graphical user interface, effects, heads up display,etc. "
@@ -35,11 +32,11 @@ const projects = [
         id: 3,
         title: "Spirit Bathouse",
         media: [
-            { type: "image", source: "/api/placeholder/600/400" },
-            { type: "image", source: "/api/placeholder/600/400" },
-            { type: "video", source: "/api/placeholder/600/400", poster: "/api/placeholder/600/400" }
+            { type: "video", source: "SpiritBathhouse.mp4" },
+            { type: "image", source: "GGJ2025.jpg" },
+            { type: "image", source: "GGJ_Award2.jpg" }
         ],
-        description: "Spirit Bathhouse is a roguelite deck-building game inspired by Spirited Away, created during a game jam with fellow developers. Players must race against time to bathe spirits, keep them happy to earn tips,"
+        description: "In the 2025 Global Game Jam, where we had less than 3 days to make a game, my team and I made Spirit Bathhouse. Spirit Bathouse is a roguelite deck-building game inspired by Spirited Away, created during a game jam with fellow developers. Players must race against time to bathe spirits, keep them happy to earn tips,"
         + " and manage their exhaustion. By strategizing card combos, upgrading your deck, and meeting daily quotas, you ensure the bathhouse runs smoothly. Unlike traditional deck-builders that focus on combat, Spirit Bathhouse "
         + "shifts the challenge to resource management, immersing players in the experience of running a magical bathhouse rather than battling enemies.",
         technologies: ["Godot", "GDScript"],
@@ -50,9 +47,9 @@ const projects = [
         id: 4,
         title: "Assembly Game",
         media: [
-            { type: "video", source: "/api/placeholder/600/400", poster: "/api/placeholder/600/400" },
-            { type: "image", source: "/api/placeholder/600/400" },
-            { type: "image", source: "/api/placeholder/600/400" }
+            { type: "video", source: "AssemblyGame.mp4" },
+            { type: "image", source: "AssemblerRunTimeSample.PNG" },
+            { type: "image", source: "AssemblerSampleCode.PNG" }
         ],
         description: "",
         technologies: [],
@@ -61,11 +58,10 @@ const projects = [
     },
     {
         id: 5,
-        title: "JavaScript Detective Game",
+        title: "PWA JavaScript Detective Game",
         media: [
-            { type: "image", source: "/api/placeholder/600/400" },
-            { type: "image", source: "/api/placeholder/600/400" },
-            { type: "image", source: "/api/placeholder/600/400" }
+            { type: "video", source: "DetectiveGame1.mp4" },
+            { type: "video", source: "DetectiveGameMobile.mp4" }
         ],
         description: "",
         technologies: [],
